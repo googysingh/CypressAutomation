a7b088cd08618977291f7a61f65f026eb7c64573
Gurpreet Singh <googysingh@gmail.com>
2019-11-25 18:19:23 +0530

    Created Project

---
+++ CypressAutomation/cypress/integration/examples/Test1.js
@@ -0,0 +1,13 @@
+describe("My First Test Suite", function()
+{
+    it("My 1st Test case", function()
+    {
+        cy.visit("http://www.qaclickacademy.com");
+    })
+
+    it("My 2nd Test case", function()
+    {
+        //test step
+    })
+
+})
