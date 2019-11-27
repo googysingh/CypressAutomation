7500fa5d2faf2ef73b4d2fb98659e55f627682a1
Gurpreet Singh <googysingh@gmail.com>
2019-11-27 11:09:28 +0530

    Day 2

--- CypressAutomation/cypress/integration/examples/Test1.js
+++ CypressAutomation/cypress/integration/examples/Test1.js
@@ -1,13 +1,26 @@
+/// <reference types="Cypress" />
 describe("My First Test Suite", function()
 {
     it("My 1st Test case", function()
     {
-        cy.visit("http://www.qaclickacademy.com");
-    })
+        cy.visit("http://qaclickacademy.com/practice.php");
+        //cy.get("#displayed-text").type("This is first type");
+        cy.wait(1000);
+        cy.get("#hide-textbox").click();
+        cy.wait(1000);
+        cy.get("#show-textbox").click();
+        cy.wait(1000);
 
+    })
     it("My 2nd Test case", function()
     {
-        //test step
+        cy.get('#displayed-text').type('This is first type');
+        cy.wait(1000);
+        cy.get('#hide-textbox').click();
+        cy.wait(1000);
+        cy.get('#show-textbox').click();
+        cy.wait(1000);
+        //cy.get('h1').should("be.a","Practice Page");
     })
 
 })
